9f3766ef8c56d6f214226c6c5fc0707c628c23fb
2016024784 <2016024784@hanyang.ac.kr>
2020-11-04 17:31:14 +0900

    Lock

---
+++ project4/include/lock_table.h
@@ -0,0 +1,35 @@
+#ifndef __LOCK_TABLE_H__
+#define __LOCK_TABLE_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <unordered_map>
+
+using namespace std;
+
+typedef struct lock_t lock_t;
+typedef struct lock_table_entry lock_table_entry;
+
+
+struct lock_t {
+	lock_table_entry * sentinel;
+	lock_t * prev;
+	lock_t * next;
+	pthread_cond_t cond;
+};
+
+struct lock_table_entry {
+	int table_id;
+	int64_t key;
+	lock_t * head;
+	lock_t * tail;
+};
+
+
+/* APIs for lock table */
+int init_lock_table();
+lock_t* lock_acquire(int table_id, int64_t key);
+int lock_release(lock_t* lock_obj);
+
+#endif /* __LOCK_TABLE_H__ */
---
+++ project4/src/lock_table.cpp
@@ -0,0 +1,83 @@
+#include <lock_table.h>
+
+unordered_map<int64_t, lock_table_entry> lock_hash_table[11];
+pthread_mutex_t lock_table_latch;
+
+int k = 0;
+
+
+int init_lock_table() {
+	int result;
+	result = pthread_mutex_init(&lock_table_latch, NULL);
+	if (result != 0) printf("pthread_mutex_init() error\n");
+	return 0;
+}
+
+
+lock_t * lock_acquire(int table_id, int64_t key) {
+
+	pthread_mutex_lock(&lock_table_latch);
+	
+	lock_table_entry table_entry;
+	lock_t * lock_obj;
+
+	lock_obj = (lock_t*)malloc(sizeof(lock_t));
+	if (lock_obj == NULL) printf("lock malloc error\n");
+
+	if (lock_hash_table[table_id].find(key) == lock_hash_table[table_id].end()) {
+		
+		table_entry.table_id = table_id;
+		table_entry.key = key;
+		table_entry.head = lock_obj;
+		table_entry.tail = lock_obj;
+
+		lock_hash_table[table_id][key] = table_entry;
+
+		lock_obj->sentinel = &lock_hash_table[table_id][key];
+		lock_obj->prev = NULL;
+		lock_obj->next = NULL;
+
+	}
+
+	else {
+
+		lock_obj->sentinel = &lock_hash_table[table_id][key];
+		lock_obj->prev = lock_hash_table[table_id][key].tail;
+		lock_obj->next = NULL;
+		pthread_cond_init(&lock_obj->cond, NULL);
+
+		lock_hash_table[table_id][key].tail->next = lock_obj;
+
+		lock_hash_table[table_id][key].tail = lock_obj;
+
+		pthread_cond_wait(&lock_obj->cond, &lock_table_latch);
+
+	}
+
+	pthread_mutex_unlock(&lock_table_latch);
+
+	return lock_obj;
+}
+
+
+int lock_release(lock_t* lock_obj) {
+
+	pthread_mutex_lock(&lock_table_latch);
+
+	if (lock_obj->next == NULL) 
+		lock_hash_table[lock_obj->sentinel->table_id].erase(lock_obj->sentinel->key);
+
+	else {
+		lock_obj->sentinel->head = lock_obj->next;
+
+		lock_obj->next->prev = NULL;
+
+		pthread_cond_signal(&lock_obj->next->cond);
+	}
+
+	free(lock_obj);
+
+	pthread_mutex_unlock(&lock_table_latch);
+
+	return 0;
+}
